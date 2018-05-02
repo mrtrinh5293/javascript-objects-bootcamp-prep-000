d13d38d49be97b0da28ad5ca000c92257536e419
Duc Dang <mrtrinh5293@gmail.com>
2018-05-02 20:11:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
+<<<<<<< HEAD
   playlist[artistName] = 'songTitle'
   return playlist;
 }
@@ -11,3 +12,8 @@ function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }
+=======
+  playlist.artistName = 'songTitle'
+  return playlist;
+}
+>>>>>>> 60916a83d434a0dba278040269001093dd49a172
